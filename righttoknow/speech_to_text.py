2dcbd04e1ebdc4749d55d3bf7c29b0112bd8a601
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2024-04-08 23:17:18 -0400

    Added BaseSpeechToTextAgent and GoogleSpeechToTextAgent

--- righttoknow/speech_to_text.py
+++ righttoknow/speech_to_text.py
@@ -1,23 +1,33 @@
+from io import BytesIO
+import traceback
+
 import speech_recognition as sr
 
-class SpeechToText():
-    def __init__(self) -> None:
-        self.recognizer = sr.Recognizer()
-    
-    def convert_speech_to_text(self,audio_file_path):
-        with sr.AudioFile(audio_file_path) as source:
-            
-            # Listen for the data (load audio to memory)
-            audio_data = self.recognizer.record(source)
-            text = ""
 
-            # Recognize (convert from speech to text)
-            try:
-                text = self.recognizer.recognize_google(audio_data)
-                print(text)
-            except sr.UnknownValueError:
-                print("Google Speech Recognition could not understand audio")
-            except sr.RequestError as e:
-                print(f"Could not request results from Google Speech Recognition service; {e}")
-        
-            return text
+class BaseSpeechToTextAgent:
+    def transcribe(self, audio_stream: BytesIO, verbose: bool = False):
+        raise NotImplementedError()
+
+
+class GoogleSpeechToTextAgent(BaseSpeechToTextAgent):
+    def __init__(self):
+        self._recognizer = sr.Recognizer()
+
+    def transcribe(self, audio_stream: BytesIO, verbose: bool = False):
+        try:
+            audio_data = sr.AudioData(audio_stream, sample_rate=16000, sample_width=2)
+            transcript = self._recognizer.recognize_google(audio_data)
+
+            if verbose:
+                print(transcript)
+
+            return transcript
+
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print(f"Could not request results from Google Speech Recognition service; {e}")
+        except Exception as e:
+            traceback.print_exception(e)
+
+        return ""
